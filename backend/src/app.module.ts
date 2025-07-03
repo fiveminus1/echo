170426f240aaf6813d61b4b5c40ada37d19b7e00
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2025-07-03 16:29:37 -0700

    fix: use root for generated prisma client

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { AuthController } from './modules/auth/auth.controller';
 import { AppService } from './app.service';
+import { AuthService } from './modules/auth/auth.service';
+import { PrismaService } from 'prisma/prisma.service';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, AuthController],
+  providers: [AppService, AuthService, PrismaService, ConfigService],
 })
 export class AppModule {}
