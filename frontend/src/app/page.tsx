dda0ed1625dac12a71e1e967ef7eea0c5aa02c12
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2025-07-03 02:19:58 -0700

    feat: add files to run backend/frontend repos concurrently

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,7 @@
 'use client';
 
-import { Card, CardContent  } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { SpotifyIcon } from '@/components/icons/spotify-icon';
-import { ArrowRight } from 'lucide-react';
 
 export default function Home() {
   const handleLogin = () => {
